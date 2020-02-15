15ea861e3a79af54413520bff2c8ed6681122a2f
Francisco Thiago <fthiagomedeiros@gmail.com>
2020-02-15 17:08:08 -0300

    Project filtering endpoint

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
+# Deployment endpoint
+
+http://image-filter-api-dev.us-west-2.elasticbeanstalk.com/filteredimage?image_url=https://s3.amazonaws.com/nerit-cms/ibracam/text-editor/Globo.png
+
 # Udagram Image Filtering Microservice
 
 Udagram is a simple cloud application developed alongside the Udacity Cloud Engineering Nanodegree. It allows users to register and log into a web client, post photos to the feed, and process photos using an image filtering microservice.
--- src/server.ts
+++ src/server.ts
@@ -1,46 +1,67 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import {filterImageFromURL, deleteLocalFiles} from './util/util';
+import {deleteLocalFiles, filterImageFromURL} from './util/util';
+
+import {ResponseBody} from './response/ResponseBody';
+
+const isImageUrl = require('is-image-url');
+
+const HTTP_OK = 200;
+const HTTP_UNPROCESSABLE_ENTITY = 422;
+
 
 (async () => {
 
-  // Init the Express application
   const app = express();
-
-  // Set the network port
   const port = process.env.PORT || 8082;
-  
+
+
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
-  // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
-  // GET /filteredimage?image_url={{URL}}
-  // endpoint to filter an image from a public url.
-  // IT SHOULD
-  //    1
-  //    1. validate the image_url query
-  //    2. call filterImageFromURL(image_url) to filter the image
-  //    3. send the resulting file in the response
-  //    4. deletes any files on the server on finish of the response
-  // QUERY PARAMATERS
-  //    image_url: URL of a publicly accessible image
-  // RETURNS
-  //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
 
-  /**************************************************************************** */
+  // filteredimage endpoint
+  app.get( "/filteredimage", async ( req, res ) => {
 
-  //! END @TODO1
-  
+    // Gets the image_url request parameter
+    let image_url = req.query.image_url;
+
+    // Validate the image_url
+    if (!isImageUrl(image_url)) {
+      let response = new ResponseBody(HTTP_UNPROCESSABLE_ENTITY, 'The image_url is not valid');
+      return res.status(response.code).send(response);
+    }
+
+    // call filterImageFromURL(image_url) to filter the imageLocalPath
+    const imageLocalPath = await filterImageFromURL(image_url);
+
+    // send the resulting file in the response and delete it from server
+    res.status(HTTP_OK).sendFile(imageLocalPath, function (err) {
+      if (err) {
+      } else {
+        deleteFileFromServer(imageLocalPath);
+      }
+    });
+
+  });
+
+
   // Root Endpoint
   // Displays a simple message to the user
   app.get( "/", async ( req, res ) => {
     res.send("try GET /filteredimage?image_url={{}}")
-  } );
-  
+  });
+
 
   // Start the Server
   app.listen( port, () => {
-      console.log( `server running http://localhost:${ port }` );
-      console.log( `press CTRL+C to stop server` );
-  } );
+    console.log( `server running http://localhost:${ port }` );
+    console.log( `press CTRL+C to stop server` );
+  });
+
 })();
+
+
+function deleteFileFromServer(image: string) {
+  deleteLocalFiles( [ image ] ).then(() => console.debug('File deleted: ' + image));
+}
